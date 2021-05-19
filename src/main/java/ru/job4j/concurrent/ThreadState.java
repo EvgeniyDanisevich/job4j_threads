69f3625ab65f6e46097f1936f7792bbadbd92605
EvgeniyDanisevich <a937va@gmail.com>
2021-05-20 01:38:09 +0300

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        first.start();
+        second.start();
+
+        first.join();
+        second.join();
+
+        if (first.getState() == Thread.State.TERMINATED
+                && second.getState() == Thread.State.TERMINATED) {
+            System.out.println("Threads are terminated");
+        }
+    }
+}
