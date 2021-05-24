58dceba95390835efff89a029ebec9d60d399701
EvgeniyDanisevich <a937va@gmail.com>
2021-05-24 19:25:38 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -0,0 +1,32 @@
+package ru.job4j.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public CASCount(Integer initValue) {
+        if (initValue == null || initValue == 0) {
+            count.set(0);
+        } else {
+            count.set(initValue);
+        }
+    }
+
+    public void increment() {
+        Integer newValue;
+        Integer refValue;
+        do {
+            refValue = count.get();
+            newValue = refValue + 1;
+        }
+        while (!count.compareAndSet(refValue, newValue));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
